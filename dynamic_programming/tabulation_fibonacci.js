009c2da5ed3ee5ef36b64051eb23f1d929ed2757
Amal Prasanna <30523972+Amal1707@users.noreply.github.com>
2020-08-02 14:10:26 +0530

    Create tabulation_fibonacci.js

---
+++ dynamic_programming/tabulation_fibonacci.js
@@ -0,0 +1,11 @@
+//  Fibonacci using Tabulation method
+
+function fib(n){
+    if(n<=2) return 1;
+    var fibNums =[0, 1, 1];
+    for(var i=3; i<=n; i++){
+        fibNums[i] = fibNums[i-1] + fibNums[i-2];
+    }
+    return fibNums[n];
+
+}
